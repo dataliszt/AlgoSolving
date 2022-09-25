0df30bbcad3b2ca00212f2594d720cc7a9b333be
dataliszt <data.liszt@gmail.com>
2022-09-25 20:49:15 +0900

    solved

--- 2231.py
+++ 2231.py
@@ -1,10 +1,15 @@
 # 분해합 
 
-number = map(int, input().split())
-c_list = []
-for i in range(number - 54, number):
-    temp_num = i
-    constructor = temp_num
+number = int(input())
+if number < len(str(number)) * 9:
+    init_number = 1
+else: 
+    init_number = number - len(str(number)) * 9 
+
+c_list = [] 
+for i in range(init_number, number):
+    temp_num = i 
+    constructor = i 
     for char in str(temp_num):
         temp_num += int(char)
     if temp_num == number:
@@ -12,4 +17,4 @@ for i in range(number - 54, number):
 if len(c_list) == 0:
     print(0)
 else:
-    print(c_list[0])
+    print(sorted(c_list)[0])
