5757818dbdc988e9bf48bace58ea9675d5135617
dataliszt <data.liszt@gmail.com>
2022-09-27 01:33:00 +0900

    2022_09_26

---
+++ 1920.py
@@ -0,0 +1,25 @@
+# 수 찾기 
+
+import sys 
+n = int(sys.stdin.readline().rstrip())
+search_list = list(map(int, sys.stdin.readline().rstrip().split()))
+search_list.sort()
+
+m = int(sys.stdin.readline().rstrip())
+check_list = list(map(int, sys.stdin.readline().rstrip().split()))
+
+def binary_search(search_list, target):
+    start = 0
+    end = len(search_list) - 1
+    while start <= end:
+        center = (start + end) // 2
+        if search_list[center] == target:
+            return 1
+        elif search_list[center] > target:
+            end = center - 1
+        else:
+            start = center + 1
+    return 0
+
+for target in check_list:
+    print(binary_search(search_list, target))
