910c7387b1959d65b785ef9effc26067a399fb31
dataliszt <data.liszt@gmail.com>
2022-11-11 16:27:06 +0900

    2022_11_11

--- 2839.py
+++ 2839.py
@@ -2,40 +2,67 @@
 import sys 
 #N = int(sys.stdin.readline().strip())
 
+# def count_bag(N):
+#     temp_list = []
+#     if N % 5 == 0:
+#         return N // 5
+#     if N % 3 == 0:
+#         temp_list.append(N // 3)
+        
+#     temp = 0
+#     while True:
+#         N -= 5
+#         temp += 1
+#         if N % 3 == 0:
+#             temp += N // 3
+#             temp_list.append(temp)
+#             break
+#         if N < 0:
+#             count = -1
+#             temp_list.append(count)
+#             break 
+#     print(temp_list)
+#     if len(temp_list) == 2 and -1 not in temp_list:
+#         return min(temp_list)
+#     else:
+#         return max(temp_list)
+    
+    
 def count_bag(N):
     temp_list = []
     if N % 5 == 0:
         return N // 5
     if N % 3 == 0:
         temp_list.append(N // 3)
-        
-    temp = 0
-    while True:
-        N -= 5
-        temp += 1
-        if N % 3 == 0:
-            temp += N // 3
-            temp_list.append(temp)
-            break
-        if N < 0:
-            count = -1
-            temp_list.append(count)
-            break 
-    
-    # if len(temp_list) > 1 and -1 in temp_list:
-    #     return max(temp_list)
-    # elif len(temp_list) > 1 and -1 not in temp_list:
-    #     return min(temp_list)
-    # elif len(temp_list) == 1 and -1 not in temp_list:
-    #     return temp_list[0]
-    # else:
-    #     return -1
-    if len(temp_list) == 2 and -1 not in temp_list:
-        return min(temp_list)
-    else:
-        return max(temp_list)
+    if True:
+        new_N = N
+        temp = 0
+        while True:
+            new_N -= 5
+            if new_N < 0:
+                break 
+            temp += 1
+            if new_N % 3 == 0:
+                temp += new_N // 3
+                temp_list.append(temp)
+                break
+    if True:
+        new_N = N
+        temp = 0
+        while True:
+            new_N -= 3
+            if new_N < 0:
+                break 
+            temp += 1
+            if new_N % 5 == 0:
+                temp += new_N // 5
+                temp_list.append(temp)
+                break
+    if len(temp_list) == 0:
+        return -1 
+    return min(temp_list)
     
 if __name__ == "__main__":
-    for N in [18, 4, 6, 9, 11]:
+    for N in [4998]:
         print(count_bag(N))
 
