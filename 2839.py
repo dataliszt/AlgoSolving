1c0e00402f63a06192ba43d980d6673ed94b4284
dataliszt <data.liszt@gmail.com>
2022-11-12 16:40:13 +0900

    refactoring

--- 2839.py
+++ 2839.py
@@ -2,32 +2,6 @@
 import sys 
 #N = int(sys.stdin.readline().strip())
 
-# def count_bag(N):
-#     temp_list = []
-#     if N % 5 == 0:
-#         return N // 5
-#     if N % 3 == 0:
-#         temp_list.append(N // 3)
-        
-#     temp = 0
-#     while True:
-#         N -= 5
-#         temp += 1
-#         if N % 3 == 0:
-#             temp += N // 3
-#             temp_list.append(temp)
-#             break
-#         if N < 0:
-#             count = -1
-#             temp_list.append(count)
-#             break 
-#     print(temp_list)
-#     if len(temp_list) == 2 and -1 not in temp_list:
-#         return min(temp_list)
-#     else:
-#         return max(temp_list)
-    
-    
 def count_bag(N):
     temp_list = []
     if N % 5 == 0:
@@ -61,6 +35,23 @@ def count_bag(N):
     if len(temp_list) == 0:
         return -1 
     return min(temp_list)
+
+# refactoring 
+
+def count_bag(N):
+    count = 0
+    while True:
+        if N % 5 == 0:
+            count += N // 5 
+            break
+        N -= 3
+        if N < 0:
+            break 
+        count += 1
+    if N < 0:
+        return -1 
+    return count 
+
     
 if __name__ == "__main__":
     for N in [4998]:
